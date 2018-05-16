cbfbbdc5a0e0bafcae53dc287b931892095c6500
chanmufeng <2041857329@qq.com>
2018-05-16 15:41:46 +0800

    主程序

---
+++ heap/main.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include "MaxHeap.h"
+#include "SortTestHelper.h"
+
+int main() {
+
+	//测试最大堆的插入
+//	int N=20;
+//	int* arr=SortTestHelper::generateRandomArray(N,0,N);
+//	MaxHeap<int> maxHeap = MaxHeap<int>(20);
+//	for (int i = 0; i < N; i++) {
+//		maxHeap.insert(arr[i]);
+//	}
+//	std::cout << maxHeap.size() << std::endl;
+//	maxHeap.print();
+//	delete [] arr;
+
+//利用排序测试最大堆的弹出
+//	int N=20;
+//	int* arr=SortTestHelper::generateRandomArray(N,0,N);
+//	MaxHeap<int> maxHeap = MaxHeap<int>(20);
+//	for (int i = 0; i < N; i++) {
+//		maxHeap.insert(arr[i]);
+//	}
+//	for (int i = 0; i < N; i++) {
+//		std::cout << maxHeap.pop() << " ";
+//	}
+//	delete [] arr;
+
+	return 0;
+}
+
+
+
+
